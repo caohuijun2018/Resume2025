a057a5d99e2c2bd76565133ae3cbd00292222bfe
caohuijun2018 <caohuijun2018@gmail.com>
2025-02-15 13:56:23 +0800

    modify resume data

--- src/config/resumeData.js
+++ src/config/resumeData.js
@@ -1,97 +1,68 @@
 export const resumeData = {
     // 基本信息
     basicInfo: {
-        name: "李瑞国",
-        title: "后端开发工程师",
-        contact: [
-            {icon: 'Flag', text: '后端开发工程师'},
-            {icon: "Information", text: "李瑞国 | 男 | 24 | 上海"},
-            {icon: "Time", text: "2022.6 | 3年工作经验"},
-            {icon: "Phone", text: "19946275213"},
-            {icon: "Email", text: "aboauther@gmail.com"},
-        ],
+      name: "曹慧珺",
+      title: "前端开发工程师",
+      contact: [
+        { icon: 'Flag', text: '前端开发工程师'},
+        { icon: "Information", text: "曹慧珺｜女｜上海" },
+        { icon: "Time", text: "2022.6 | 3年工作经验" },
+        { icon: "Phone", text: "15601940829" },
+        { icon: "Email", text: "caohuijun2018@gmail.com" },
+      ],
     },
-
+  
     // 工作经历
     workExperience: [
-        // {
-        //     company: "上海菜吗网络科技有限公司",
-        //     position: "法人",
-        //     period: "2024.01 - 至今",
-        //     highlights: [
-        //         "成立公司，与前公司进行项目合作"
-        //     ],
-        // },
-        {
-            company: "齐柏林上海网络科技有限公司",
-            position: "后端开发工程师",
-            period: "2022.06 - 至今",
-            highlights: [
-                "交易所项目：核心开发人员，负责交易核心以及前后数据链路的开发与迭代",
-                "业务域：业务域后端负责人，负责业务域的迭代工作以及资源协调等事宜",
-                "基础建设：技术负责人，负责链上链下多领域多数据源的开发工作"
-            ],
-        },
+      {
+        company: "盒马（中国）有限公司",
+        position: "前端开发工程师",
+        period: "2022.07 - 至今",
+        highlights: [
+          "业务域负责人：担任业务域前端负责人，独立负责多业务域的迭代工作以及资源协调等事宜。涵盖 web 端复杂页面、移动端页面、支付宝小程序、Electron 客户端 ",
+          "基础建设：低代码及 AI 驱动无代码平台开发成员",
+          "通用打印解决方案：技术负责人，负责打印链路以及打印客户端的开发与迭代"
+        ],
+      },
     ],
-
+  
     // 项目介绍
     projects: [
-        {
-            name: "Exchange Core",
-            role: "核心开发成员",
-            techStack: ["Java", "Redis"],
-            description: [
-                "作为核心开发成员，深度参与 Exchange Core 交易所的开发与迭代。项目旨在借助 Zero-Knowledge 技术实现安全高并发高可用的去中心化现货交易系统",
-                "事件溯源与快照支持：redis事件记录和回放，具备状态快照（序列化）和恢复操作功能",
-                "支持限价市价及多种订单状态（IOC,GTC,FOK）",
-                "多核流水线处理架构设计，提高系统处理效率，支持高并发场景",
-                "事件驱动架构：通过事件源（event sourcing）和消息队列（MQ）机制，确保数据一致性并提高系统的可扩展性",
-                "通过 Redis 异步启动，使用 Redis Keyspace 监听优化数据定时清理任务，实现流量削峰及资源占用动态监控",
-                "用户订单簿快照，支持市价不同区间的用户订单快照，提高数据分析能力"
-            ],
-        },
-        {
-            name: "On-Chain Cashier",
-            role: "技术负责人",
-            techStack: ["Golang", "Redis", "Mysql"],
-            description: [
-                "作为项目负责人，实现链上合约对应的 Transaction Events 的解析处理",
-                "并发处理safe gap前后的充值事件",
-                "集成链管理进度与 event 哈希一致性校验",
-                "支持快速提现及多链状态下用户操作的状态一致性功能",
-                "Prometheus 指标监控，支持系统状态实时跟踪"
-            ],
-        },
-        {
-            name: "Infra Data Spider",
-            role: "技术负责人",
-            techStack: ["Golang", "Redis", "Mysql"],
-            description: [
-                "作为项目负责人，实现链上链下不同数据源的数据获取及落库",
-                "通用级别的爬虫框架，配置级启动新数据源爬取任务",
-                "链上数据爬取任务动态管理，多链状态下配置级启动",
-                "高并发状态下动态管理API token",
-                "Prometheus 指标监控，支持系统状态实时跟踪"
-            ],
-        }
+      {
+        name: "通用标签打印解决方案",
+        role: "技术负责人",
+        techStack: ["React", "Electron"],
+        description: [
+            "作为项目负责人，实现基于 Electron 的跨端桌面打印应用，打通浏览器到客户端的数据链路，实际落地多业务场景",
+            "使用 WebSocket 实现浏览器和客户端通信",
+            "完善的任务调度管理架构",
+            "兼容多系统，支持客户端应用常规功能"
+        ],
+      },
+      {
+        name: "AI 驱动代码生成平台",
+        role: "核心开发成员",
+        techStack: ["React", "Electron"],
+        description: [
+            "作为核心开发成员，深度参与 AI 驱动代码生成平台的开发与迭代。该项目旨在借助大模型实现 P2C 功能，以显著提升开发效率。",
+            "基于 LangGraph 构建多模态解析与代码生成架构，涵盖需求文档多模态解析与格式化、API 文档解析、通用模块匹配与业务组件择取、文件代码生成、在线预览及代码库写入等复杂链路。",
+            "抽离高复用性与代表性模块构建 RAG，并结合向量数据库，有效提高代码生成准确率"
+        ],
+      },
     ],
-
+  
     // 技能清单
     skills: [
-        {description: '熟练使用 Golang 进行高并发、高性能系统开发，具备良好的代码规范和工程化能力'},
-        {description: '熟悉 Go 语言的并发编程（goroutine、channel、sync 包等）'},
-        {description: '掌握 Golang 内存管理，熟悉GC 机制、逃逸分析，能够降低 GC 压力提升系统吞吐量'},
-        {description: '熟练使用 Redis，掌握 高效的 key 设计、Stream 处理、事务管理，优化 QPS 和延迟'},
-        {description: '熟悉 Prometheus + Grafana 监控体系，能够设计业务监控，优化运维可观测性'},
-        {description: '了解 Docker & Kubernetes，具备容器化部署、服务编排、自动扩缩容经验'},
-        {description: '具备 Ethereum 相关开发经验，熟悉 Go-ethereum（Geth）、JSON-RPC、FilterLogs 监听 等技术'},
-        {description: '具备 多链交互经验（如 BSC、Polygon、Arbitrum），熟悉 EVM 兼容链的事件解析与数据存储'}
-    ],
+      { description: '熟悉 JavaScript 核心基础，掌握 ES6 及以上语法，熟练使用 TypeScript' },
+      { description: '能够编写语意化 HMTL、模块化 CSS，熟练使用 styled-components' },
+      { description: '熟练使用 React、Mobx 构建 Web 页面' },
 
+    ],
+  
     // 教育经历
     education: {
-        university: "上海海事大学",
-        degree: "计算机科学与技术",
-        period: "2018～2022 ",
+      university: "江苏科技大学",
+      degree: "信息安全",
+      period: "2018～2022 ",
     },
-};
+  };
